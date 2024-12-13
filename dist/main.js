830cd7ee466847cc6eb256ebe0c125ec22d25596
นิว กรีสุภา <xkoruzxth@gmail.com>
2024-12-13 14:01:33 +0700

    make it better

--- dist/main.js
+++ dist/main.js
@@ -5,6 +5,7 @@ const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors();
     app.useGlobalPipes(new common_1.ValidationPipe());
     await app.listen(3000);
 }
